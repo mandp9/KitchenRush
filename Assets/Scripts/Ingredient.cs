295be5dd601548981458b6b618d69ea86159abb7
Ndahai Arenas <gumojammbo@gmail.com>
2026-05-23 16:03:52 +0300

    Added pizza ingredients and spawners

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -7,6 +7,12 @@ public enum IngredientType
     Lettuce,  // 2
     Onion,    // 3
     Cheese,   // 4
+    PizzaCrust,
+    Pepperoni,
+    Mushroom,
+    Olive,
+    Ham,
+    Mozarella,
     BaseBread = 99,
     TopBread  = 100,
     // put unimplemented ingredients here
